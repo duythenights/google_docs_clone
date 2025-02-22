7d0d2726fce8e598e72a58674a3dcec8dab63947
Richard_DinhHoangDuy <dinhoangduy@gmail.com>
2025-02-22 19:43:48 +0700

    feat: tiptap extensions

--- src/app/documents/[documentId]/Editor.tsx
+++ src/app/documents/[documentId]/Editor.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { EditorContent, useEditor } from "@tiptap/react";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 export default function Editor() {
   const editor = useEditor({
@@ -8,12 +16,45 @@ export default function Editor() {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px",
         class:
-          "focus:outline-none border-[#C7c7c7] border bg-white w-[816px] min-h-[1054px] pt-10 pr-14 pb-10 cursor-text flex flex-col",
+          "tiptap focus:outline-none border-[#C7c7c7] border bg-white w-[816px] min-h-[1054px] pt-10 pr-14 pb-10 cursor-text flex flex-col",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+    ],
+    content: `
+       <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+        `,
   });
+
+  if (!editor) {
+    return null;
+  }
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:px-0  print:bg-white print:overflow-visible">
